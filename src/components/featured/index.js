a700fb246eb768b80b647811f70983b392e132c5
migellars <migellars@gmail.com>
2019-05-25 20:52:05 +0100

    initial commit

---
+++ src/components/featured/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Featured = () => {
+    return(
+        <div style={{position:'relative'}}>
+            
+            <div className="artist_name">
+                <div className='wrapper'>
+                    Ariana Grande
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
