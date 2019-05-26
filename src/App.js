0999603e12cd5ac05912485d955b76c98b4b251c
migellars <migellars@gmail.com>
2019-05-26 01:57:19 +0100

    Added venueNfo and Highlights section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
-import Featured from './components/featured/index'
-
+import Featured from './components/featured'
+import VenueNfo from './components/venueNfo';
+import Highlights from './components/Highlights';
 
 function App() {
   return (
     <div className="App" style={{ height: '1500px', background:'cornflowerblue'}}>
       <Header/>
       <Featured/>
+      <VenueNfo/>
+      <Highlights/>
     </div>
   );
 }
---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className='center_wrapper'>
+                <h2>Highlights</h2>
+                <div className='highlight_description'>
+                Lorem Ipsum shit is always available as a template, i wouldnt dare to try memorize that shit it ain't none of my concern, y'all mofo's should go shove up it your ass.
+                </div>
+            </div>
+        </Fade>
+        
+    )
+}
+
+export default Description
